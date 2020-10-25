9e19d6ac72a8917aead22e4bf8170a2c8f6c29aa
Vsevolod Rychkov <vsevolod007@gmail.com>
2020-10-26 02:08:05 +0500

    started implementing directory functionality

--- src/audio_merger/directory.py
+++ src/audio_merger/directory.py
@@ -1,5 +1,7 @@
 import os
 
+from src.audio_merger.file import File
+
 
 class Directory:
     def __init__(self, path):
@@ -8,3 +10,8 @@ class Directory:
         if not os.path.isdir(path):
             raise TypeError('Path provided is not a directory', path)
         self.name = os.path.basename(path)
+
+        self.files = []
+        files = os.listdir(path)
+        for file in files:
+            self.files.append(File(os.path.join(path, file)))
